7350dc1fe4c2c4b572b40db37ab5c9a63d1e5b3c
edusebass <eduardoalmachi123@hotmail.com>
2023-05-29 19:34:42 -0500

    Creacion de clase, constructor y getters and setters

---
+++ src/Pelicula.java
@@ -0,0 +1,59 @@
+public class Pelicula {
+    String titulo;
+    String genero;
+    int duracion;
+    int anioLanzamiento;
+    String director;
+
+    //constructor
+
+    public Pelicula(String titulo, String genero, int duracion, int anioLanzamiento, String director) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracion = duracion;
+        this.anioLanzamiento = anioLanzamiento;
+        this.director = director;
+    }
+
+    //getters and setters
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public int getAnioLanzamiento() {
+        return anioLanzamiento;
+    }
+
+    public void setAnioLanzamiento(int anioLanzamiento) {
+        this.anioLanzamiento = anioLanzamiento;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+}
