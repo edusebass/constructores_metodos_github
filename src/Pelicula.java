38c069eb48c7823d19dd5f1f4b0b3d8e5056570f
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-06-02 11:00:04 -0500

    UPDATE

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,4 +1,5 @@
 public class Pelicula {
+    //atributos
     String titulo;
     String genero;
     int duracion;
@@ -6,7 +7,6 @@ public class Pelicula {
     String director;
 
     //constructor
-
     public Pelicula(String titulo, String genero, int duracion, int anioLanzamiento, String director) {
         this.titulo = titulo;
         this.genero = genero;
@@ -16,47 +16,51 @@ public class Pelicula {
     }
 
     //getters and setters
-
     public String getTitulo() {
+
         return titulo;
     }
-
     public void setTitulo(String titulo) {
+
         this.titulo = titulo;
     }
 
     public String getGenero() {
         return genero;
     }
-
     public void setGenero(String genero) {
+
         this.genero = genero;
     }
 
     public int getDuracion() {
+
         return duracion;
     }
-
     public void setDuracion(int duracion) {
+
         this.duracion = duracion;
     }
 
     public int getAnioLanzamiento() {
+
         return anioLanzamiento;
     }
-
     public void setAnioLanzamiento(int anioLanzamiento) {
+
         this.anioLanzamiento = anioLanzamiento;
     }
 
     public String getDirector() {
+
         return director;
     }
-
     public void setDirector(String director) {
+
         this.director = director;
     }
 
+    //imprimir datos
     public void imprimirDatos(){
         System.out.println("Titulo: " + this.titulo);
         System.out.println("Género: " + this.genero);
